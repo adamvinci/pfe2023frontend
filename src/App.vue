df773df75748fc0140266e1f3aabfa15401b2497
kubraArs <kubra.arslan@student.vinci.be>
2023-12-05 17:26:10 +0100

    add LoginPage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <router-link to="/"> Home</router-link>
+  <router-link to="/home"> Home</router-link>
+  <router-link to="/"> Login</router-link>
+
 <router-view/>
 </template>
 
 <script setup>
 </script>
-
 
 
 <style scoped>
