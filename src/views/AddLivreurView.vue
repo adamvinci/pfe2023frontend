1c5229f6afcc617412b5da571216835f4b3458e1
kubraArs <kubra.arslan@student.vinci.be>
2023-12-06 10:27:13 +0100

    correction css

---
+++ src/views/AddLivreurView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="addLivreur">
+      <h1> Ajouter un Livreur </h1>
+        Nom<input type="text" v-model="nom" placeholder="Michel"/>
+        Mot de passe<input type="password" v-model="password" placeholder="*****"/>
+        <button id="boutonId" v-on:click="ajouterLivreur"> Ajouter </button>
+    </div>
+  </template>
+  
+  <script>
+export default {
+  name: 'AddLivreurView',
+  data() {
+    return {
+      nom: '',
+      password: '',
+    };
+  },
+  methods: {
+    async ajouterLivreur() {
+          this.$router.push('/');
+    }   
+}     
+};
+</script>
---
+++ src/views/AssignerView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="assigner">
+      <form @submit.prevent="submitCommand">
+        <div class="table-container">
+          <h1>Assigner commande à un livreur</h1>
+          <label id="list" for="client">Liste clients :</label>
+          <select v-model="selectedClient" id="client">
+            <option value="client1">Client1</option>
+            <option value="client2">Client2</option>
+          </select>
+          <label id="list" for="livreur">Liste livreur :</label>
+          <select v-model="selectedLivreur" id="livreur">
+            <option value="livreur1">Livreur1</option>
+            <option value="livreur2">Livreur2</option>
+          </select>
+          <button id="boutonId" type="submit">Assigner</button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'assigner',
+    data() {
+      return {
+        selectedClient: 'client1',
+        selectedLivreur: 'livreur1',
+      };
+    },
+    methods: {
+      submitCommand() {
+        // Vous pouvez traiter les valeurs sélectionnées ici
+        console.log('Client sélectionné:', this.selectedClient);
+        console.log('Livreur sélectionné:', this.selectedLivreur);
+        this.$router.push('/home');
+    },
+    },
+  };
+  </script>
