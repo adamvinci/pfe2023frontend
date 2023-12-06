d2dfea531dcdf2da4013f74c094be33ca5220208
kubraArs <kubra.arslan@student.vinci.be>
2023-12-06 10:27:04 +0100

    correction css

--- src/views/AddCommandView.vue
+++ src/views/AddCommandView.vue
@@ -1,121 +1,84 @@
 <template>
-    <div class="addCommand">
-  
-      <form @submit.prevent="submitCommand">
-        <div class="table-container1">
-         <h1>Ajouter Commande</h1>
+   <div class="addCommand">
+    <form @submit.prevent="submitCommand">
+      <div class="table-container">
+        <h1>Ajouter Commande</h1>
 
-          <table>
-            <thead>
-              <tr>
-                <th>Article</th>
-                <th>Quantité</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Langes</td>
-                <td>
-                  <input type="number" v-model="langesQuantity" />
-                </td>
-              </tr>
-              <tr>
-                <td>Inserts</td>
-                <td>
-                  <input type="number" v-model="insertsQuantity" />
-                </td>
-              </tr>
-              <tr>
-                <td>Sacs</td>
-                <td>
-                  <input type="number" v-model="sacsQuantity" />
-                </td>
-              </tr>
-              <tr>
-                <td>Gants</td>
-                <td>
-                  <input type="number" v-model="gantsQuantity" />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <button type="submit">Ajouter</button>
-        </div>
-  
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'addCommand',
-    data() {
-      return {
-        langesQuantity: 0,
-        insertsQuantity: 0,
-        sacsQuantity: 0,
-        gantsQuantity: 0,
-      };
+        <label for="day">Jour de la semaine :</label>
+        <select v-model="selectedDay" id="day">
+          <option value="lundi">Lundi</option>
+          <option value="mardi">Mardi</option>
+          <option value="mercredi">Mercredi</option>
+          <option value="jeudi">Jeudi</option>
+          <option value="vendredi">Vendredi</option>
+          <option value="samedi">Samedi</option>
+          <option value="dimanche">Dimanche</option>
+        </select>
+
+        <table id="table">
+          <thead>
+            <tr>
+              <th>Article</th>
+              <th>Quantité</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Langes</td>
+              <td>
+                <input type="number" v-model="langesQuantity" />
+              </td>
+            </tr>
+            <tr>
+              <td>Inserts</td>
+              <td>
+                <input type="number" v-model="insertsQuantity" />
+              </td>
+            </tr>
+            <tr>
+              <td>Sacs</td>
+              <td>
+                <input type="number" v-model="sacsQuantity" />
+              </td>
+            </tr>
+            <tr>
+              <td>Gants</td>
+              <td>
+                <input type="number" v-model="gantsQuantity" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button id="boutonId" type="submit" @click="submitCommand">Ajouter</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addCommand',
+  data() {
+    return {
+      langesQuantity: 0,
+      insertsQuantity: 0,
+      sacsQuantity: 0,
+      gantsQuantity: 0,
+      selectedDay: 'lundi', // Valeur par défaut
+    };
+  },
+  methods: {
+    submitCommand() {
+      // Vous pouvez traiter les quantités et le jour ici
+      console.log('Jour de la semaine:', this.selectedDay);
+      console.log('Langes:', this.langesQuantity);
+      console.log('Inserts:', this.insertsQuantity);
+      console.log('Sacs:', this.sacsQuantity);
+      console.log('Gants:', this.gantsQuantity);
+
+      // Rediriger vers /home
+      this.$router.push('/home');
     },
-    methods: {
-      submitCommand() {
-        // Vous pouvez traiter les quantités ici
-        console.log('Langes:', this.langesQuantity);
-        console.log('Inserts:', this.insertsQuantity);
-        console.log('Sacs:', this.sacsQuantity);
-        console.log('Gants:', this.gantsQuantity);
-  
-        // Réinitialiser les quantités après la soumission
-        this.langesQuantity = 0;
-        this.insertsQuantity = 0;
-        this.sacsQuantity = 0;
-        this.gantsQuantity = 0;
-      },
-    },
-  };
+  },
+};
   </script>
-  
-  <style>
- 
-  
-  .table-container1 {
-    overflow: auto;
-    margin-bottom: 20px;
-    flex-direction: column;
-    align-items: center;
-    width: 40%;
-    margin: auto;
-    text-align: center;
-    padding: 50px;
-    border: 1px solid #ccc;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    background: #E1DAFB;
-  }
-  
-  table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-bottom: 10px;
-  }
-  
-  table, th, td {
-    border: 1px solid #ddd;
-  }
-  
-  th, td {
-    padding: 10px;
-    text-align: center;
-  }
-  .table-container1 button {
-      width: 50%;
-      height: 40px;
-      border: 1px solid #C4B4FD;
-      background: #B09CFB;
-      color: #fff;
-      cursor: pointer;
-      border-radius: 3px;
-      border-radius: 15px;
-  }
-  </style>
-  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="table-container">
-    <div class="table-wrapper">
-      <table>
+  <div class="table-container" id="homeView">
+    <div class="table-wrapper" id="homeViewDiv">
+      <table id="tableHomeView">
         <thead>
           <tr>
             <th>Client</th>
@@ -15,7 +15,7 @@
               <router-link :to="{ path: '/' }">{{ client }}</router-link>
             </td>
             <td>
-              <button
+              <button id="livraisonCheck"
                 @click="toggleLivraison(index)"
                 :style="{ color: livraisons[index] ? 'green' : 'red' }"
               >
@@ -39,48 +39,3 @@ const toggleLivraison = (index) => {
   livraisons.value[index] = !livraisons.value[index];
 };
 </script>
-
-<style scoped>
-/* Styles pour le conteneur principal */
-.table-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 4rem; /* Ajuste la marge en haut pour rapprocher du navbar */
-}
-
-/* Styles pour le wrapper du tableau */
-.table-wrapper {
-  border: 2px solid #ddd; /* Bordure du carré */
-  padding: 20px; /* Espace intérieur du carré */
-  border-radius: 10px; /* Coins arrondis du carré */
-}
-
-/* Styles pour le tableau */
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-th, td {
-  border: 1px solid #ddd;
-  padding: 8px;
-  text-align: left;
-}
-
-th {
-  background-color: #f2f2f2;
-}
-
-/* Styles pour les boutons */
-button {
-  padding: 6px 10px;
-  cursor: pointer;
-  border: none;
-  border-radius: 4px;
-}
-
-button:hover {
-  opacity: 0.8;
-}
-</style>
