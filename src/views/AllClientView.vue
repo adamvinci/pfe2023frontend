153e84dc784b0e6797e298b5ce9fa10bc52d091c
leonkelmendi1 <83784260+leonkelmendi1@users.noreply.github.com>
2023-12-07 14:29:04 +0100

    fix bug

--- src/views/AllClientView.vue
+++ src/views/AllClientView.vue
@@ -1,105 +1,106 @@
 <template>
-  <div class="page-container">
-    <div class="table-container" id="homeView">
-      <div class="table-wrapper" id="homeViewDiv">
-        <table id="tableHomeView">
-          <thead>
-            <tr>
-              <th class="hidden-id">Hidden ID</th>
-              <th>Client</th>
-              <th>Ville</th>
-              <th>Adresse</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(livraison, index) in livraisons" :key="index">
-              <td class="hidden-id">{{ livraison.hiddenId }}</td>
-              <td>{{ livraison.client }}</td>
-              <td>{{ livraison.ville }}</td>
-              <td>{{ livraison.adresse }}</td>
-            </tr>
-          </tbody>
-        </table>
+    <div class="page-container">
+      <div class="table-container" id="homeView">
+        <div class="table-wrapper" id="homeViewDiv">
+          <table id="tableHomeView">
+            <thead>
+              <tr>
+                <th class="hidden-id">Hidden ID</th>
+                <th>Client</th>
+                <th>Ville</th>
+                <th>Adresse</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- Accédez à la propriété "creches" de l'objet réactif -->
+              <tr v-for="(livraison, index) in livraisons.creches" :key="index">
+                <td class="hidden-id">{{ livraison.id }}</td>
+                <td>{{ livraison.nom }}</td>
+                <td>{{ livraison.gsm }}</td>
+                <td>{{ livraison.adresse }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
-</template>
+  </template>
   
-<script setup>
-import { ref, onMounted } from 'vue';
-
-let livraisons = [];
-const accessToken = localStorage.getItem('accessToken');
-console.log(accessToken);
-
-const fetchData = async () => {
-  try {
-    const response = await fetch('/api/creches', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}`,
-      },
-    });
-    if (response.ok) {
-      livraisons = await response.json();
-      console.log(livraisons)
-    } else {
-      console.error('Erreur lors de la récupération des données:', response.status);
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  
+  // Utilisez la fonction ref pour déclarer la liste
+  const livraisons = ref({});
+  const accessToken = localStorage.getItem('accessToken');
+  
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/creches', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      });
+      if (response.ok) {
+        // Mettez à jour la valeur de livraisons avec la nouvelle liste
+        livraisons.value = await response.json();
+        console.log(livraisons.value);
+      } else {
+        console.error('Erreur lors de la récupération des données:', response.status);
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données:', error);
     }
-  } catch (error) {
-    console.error('Erreur lors de la récupération des données:', error);
-  }
-};
-
-onMounted(() => {
-  fetchData();
-});
-</script>
-
+  };
   
-<style scoped>
-.hidden-id {
-  display: none;
-}
-
-#homeView {
-  overflow: auto;
-  margin-bottom: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 40%;
-  margin: auto;
-  text-align: center;
-  padding: 50px;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  background: #E1DAFB;
-  margin-top: 4rem;
-}
-
-#homeViewDiv {
-  border: 2px solid #ddd;
-  padding: 20px;
-  border-radius: 10px;
-}
-
-#tableHomeView {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-#tableHomeView th,
-td {
-  border: 1px solid #ddd;
-  padding: 8px;
-  text-align: left;
-}
-
-#tableHomeView th {
-  background-color: #f2f2f2;
-}
-</style>
+  onMounted(() => {
+    fetchData();
+  });
+  </script>
+  
+  <style scoped>
+  .hidden-id {
+    display: none;
+  }
+  
+  #homeView {
+    overflow: auto;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 40%;
+    margin: auto;
+    text-align: center;
+    padding: 50px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background: #E1DAFB;
+    margin-top: 4rem;
+  }
+  
+  #homeViewDiv {
+    border: 2px solid #ddd;
+    padding: 20px;
+    border-radius: 10px;
+  }
+  
+  #tableHomeView {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  
+  #tableHomeView th,
+  td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+  }
+  
+  #tableHomeView th {
+    background-color: #f2f2f2;
+  }
+  </style>
   
