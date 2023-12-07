1787199993d98d934d8362ef8be12a88d8237055
kubraArs <kubra.arslan@student.vinci.be>
2023-12-07 13:28:17 +0100

    add backend addLivreur

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -17,6 +17,7 @@ const email = ref('');
 const password = ref('');
 const errorMessage = ref('');
 const $router = useRouter();
+const accessToken = ref(''); // Variable pour stocker le token d'accès
 
 const login = async () => {
   try {
@@ -34,9 +35,25 @@ const login = async () => {
       }),
     });
 
+    // Vérification de la conservation du token
+    console.log(localStorage.getItem('accessToken'));
+
     if (response.ok) {
-      // Utilisation de la navigation avec la référence à $router
+      const data = await response.json();
+      console.log('Réponse du serveur:', data);
+
+      accessToken.value = data.token;
+      console.log('Token d\'accès:', accessToken.value);
+
+      // Stocke le token dans le localStorage
+      localStorage.setItem('accessToken', accessToken.value);
+
+      // Ajoute le token aux en-têtes des futures requêtes
+      setAuthorizationHeader(accessToken.value);
+
+      // Le reste de votre code reste inchangé
       $router.push('/home');
+
     } else {
       // Affichage du message d'erreur
       errorMessage.value = 'La connexion a échoué. Vérifiez vos identifiants.';
@@ -48,7 +65,22 @@ const login = async () => {
     console.error('Erreur lors de la connexion:', error);
   }
 };
+
+const setAuthorizationHeader = (token) => {
+  // Ajoute le token aux en-têtes des futures requêtes
+  // Vous devrez ajuster cela en fonction de la manière dont votre application gère les requêtes
+  // Par exemple, en utilisant Axios, vous feriez axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  fetch('http://localhost:3333/creches', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    mode: 'cors',
+  });
+};
 </script>
+
 <style scoped>
 /* Style pour LoginView*/
 .error-message {
@@ -70,8 +102,8 @@ const login = async () => {
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   background: #E1DAFB;
   margin-top: 4rem; /* Ajuste la marge en haut pour rapprocher du navbar */
-
 }
+
 .login h1{
   font-size: 24px; /* Vous pouvez ajuster la taille selon vos préférences */
   margin: auto;
@@ -82,6 +114,7 @@ const login = async () => {
   margin-bottom: 20px;
   text-shadow: 4px 3px 0px #fff, 9px 8px 0px rgba(0, 0, 0, 0.15);
 }
+
 .login input {
   width: 70%;
   height: 40px;
@@ -92,6 +125,7 @@ const login = async () => {
   box-sizing: border-box;
   background-color: #D3CAF6;
 }
+
 h1 {
   font-size: 24px; /* Vous pouvez ajuster la taille selon vos préférences */
   margin: auto;
