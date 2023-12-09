02547b8235e5a159a680166956bf761456dccdef
adam <adame.barhdadi@student.vinci.be>
2023-12-09 20:56:15 +0100

    add error handling

--- src/views/AddLivreurView.vue
+++ src/views/AddLivreurView.vue
@@ -3,7 +3,7 @@
     <h1> Ajouter un Livreur </h1>
     Nom<input type="text" v-model="nom" placeholder="Michel" />
     Mot de passe<input type="password" v-model="password" placeholder="*****" />
-    <Bouton @click="ajouterLivreur"> Ajouter </Bouton>
+    <button @click="ajouterLivreur"> Ajouter </button>
     <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
     <p v-if="successMessage" class="success-message">{{ successMessage }}</p>
   </div>
@@ -19,6 +19,7 @@ const password = ref('');
 const errorMessage = ref('');
 const successMessage = ref('');
 const $router = useRouter();
+const accessToken = localStorage.getItem('accessToken');
 
 const ajouterLivreur = async () => {
   try {
@@ -26,8 +27,8 @@ const ajouterLivreur = async () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
       },
-      mode: 'cors',
       body: JSON.stringify({
         nom: nom.value,
         password: password.value,
@@ -37,11 +38,11 @@ const ajouterLivreur = async () => {
     if (response.ok) {
       successMessage.value = 'Livreur ajouté avec succès!';
       errorMessage.value = '';
-      $router.push('/');
     } else {
-      errorMessage.value = 'Erreur lors de l\'ajout du livreur. Veuillez réessayer.';
+      const errors = await response.json();
+      errorMessage.value = errors;
+      // errorMessage.value = 'Erreur lors de l\'ajout du livreur. Veuillez réessayer.';
       successMessage.value = '';
-      console.error('Erreur lors de l\'ajout du livreur:', response.status);
     }
   } catch (error) {
     errorMessage.value = 'Erreur lors de l\'ajout du livreur. Veuillez réessayer.';
@@ -57,10 +58,12 @@ const ajouterLivreur = async () => {
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 90%; /* Ajustement de la largeur pour les smartphones */
+  width: 90%;
+  /* Ajustement de la largeur pour les smartphones */
   margin: auto;
   text-align: center;
-  padding: 20px; /* Ajustement de l'espace intérieur pour les smartphones */
+  padding: 20px;
+  /* Ajustement de l'espace intérieur pour les smartphones */
   border: 1px solid #ccc;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -70,10 +73,13 @@ const ajouterLivreur = async () => {
 }
 
 .addLivreur input {
-  width: 100%; /* Remplir la largeur pour les smartphones */
+  width: 100%;
+  /* Remplir la largeur pour les smartphones */
   height: 40px;
-  padding: 10px; /* Ajustement de la taille du padding pour les smartphones */
-  margin-bottom: 10px; /* Espace entre les champs pour les smartphones */
+  padding: 10px;
+  /* Ajustement de la taille du padding pour les smartphones */
+  margin-bottom: 10px;
+  /* Espace entre les champs pour les smartphones */
   border: 1px solid #ddd;
   border-radius: 15px;
   box-sizing: border-box;
@@ -82,19 +88,22 @@ const ajouterLivreur = async () => {
 }
 
 .addLivreur h1 {
-  font-size: 20px; /* Ajustement de la taille de la police pour les smartphones */
+  font-size: 20px;
+  /* Ajustement de la taille de la police pour les smartphones */
   margin: auto;
   text-align: center;
   font-weight: bold;
   text-transform: uppercase;
   letter-spacing: 2px;
-  margin-bottom: 10px; /* Réduction de la marge en bas pour les smartphones */
+  margin-bottom: 10px;
+  /* Réduction de la marge en bas pour les smartphones */
   text-shadow: 4px 3px 0px #fff, 9px 8px 0px rgba(0, 0, 0, 0.15);
 }
 
 .error-message,
 .success-message {
-  font-size: 12px; /* Ajustement de la taille de la police pour les smartphones */
+  font-size: 12px;
+  /* Ajustement de la taille de la police pour les smartphones */
   margin-top: 10px;
 }
 </style>
