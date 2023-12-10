9d335dbd6f9b73115933f80941de2f3b7413843c
kubraArs <kubra.arslan@student.vinci.be>
2023-12-10 16:18:03 +0100

    créer tournée

--- src/views/AllClientView.vue
+++ src/views/AllClientView.vue
@@ -6,7 +6,16 @@
     <button v-if="editMode" @click="saveChanges" class="btn-enregistrer">
       Enregistrer
     </button>
+    <div v-if="editMode" class="form-container">
+      <h3>Créer une nouvelle tournée</h3>
+      
+      <form @submit.prevent="createTournee">
+        <label>Nom de la tournée:</label>
+        <input v-model="newTournee" type="text" required />
+        <button type="submit">Créer</button>
+      </form>
 
+    </div>
     <div class="table-container" id="homeView">
       <div class="table-wrapper" id="homeViewDiv">
         <table id="tableHomeView">
@@ -16,7 +25,9 @@
               <th>Client</th>
               <th>Num tel</th>
               <th>Adresse</th>
-              <th v-if="editMode">Action</th>
+              <th v-if="editMode">Choix</th>
+              <th>Tournee</th>
+
             </tr>
           </thead>
           <tbody>
@@ -28,29 +39,50 @@
               <td v-if="editMode">
                 <input type="checkbox" v-model="livraison.selected" />
               </td>
+              <td>
+                {{
+                  (livraison.id &&
+                    apiTournees.find(
+                      (tournee) => tournee.creches.some((c) => c.id === livraison.id)
+                    )?.nom) ||
+                  ''
+                }}
+              </td>
             </tr>
           </tbody>
+
         </table>
         <select v-if="editMode" v-model="selectedTournee">
-          <option v-for="tournee in tournees" :key="tournee">{{ tournee }}</option>
+          <option value="" disabled selected>Choisissez une tournée</option>
+          <option v-for="tournee in apiTournees" :key="tournee.id" :value="tournee.id">
+            {{ tournee.nom }}
+          </option>
         </select>
       </div>
     </div>
   </div>
 </template>
-  
+
 <script setup>
 import { ref, onMounted } from 'vue';
 
 const livraisons = ref({});
 const accessToken = localStorage.getItem('accessToken');
+const apiTournees = ref([]); // Ajout de la référence pour les tournées de l'API
+
+const tournees = ref([]);
+const livraisonss = ref(Array(tournees.value.length).fill(false));
+
+const newTournee = ref('');
+
+const selectedCrecheId = ref(null);
 
 const fetchData = async () => {
   try {
+    // Chargement des creches
     const response = await fetch(`${process.env.VUE_APP_BASEURL}/creches`, {
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json',
         'Authorization': `Bearer ${accessToken}`,
       },
     });
@@ -63,6 +95,26 @@ const fetchData = async () => {
     } else {
       console.error('Error fetching data:', response.status);
     }
+
+    // Chargement des tournées depuis l'API
+    const tourneesResponse = await fetch(`${process.env.VUE_APP_BASEURL}/tournees`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    });
+
+    if (tourneesResponse.ok) {
+      apiTournees.value = await tourneesResponse.json();
+      console.log('Tournées data:', apiTournees.value);
+
+      // Ajoute cette boucle pour afficher chaque tournée détaillée dans la console
+      for (const tournee of apiTournees.value) {
+        console.log('Nombre de tournées:', apiTournees.value.length);
+      }
+    } else {
+      console.error('Error fetching tournees:', tourneesResponse.status);
+    }
   } catch (error) {
     console.error('Fetch error:', error);
   }
@@ -70,7 +122,8 @@ const fetchData = async () => {
 
 const editMode = ref(false);
 const selectedTournee = ref('');
-const tournees = ["Tournee A", "Tournee B", "Tournee C"];
+const errorMessage = ref('');
+const successMessage = ref('');
 
 const toggleEditMode = () => {
   editMode.value = !editMode.value;
@@ -84,8 +137,50 @@ const saveChanges = () => {
 onMounted(() => {
   fetchData();
 });
+
+const createTournee = async () => {
+  try {
+    const selectedCreches = livraisons.value.creches.filter(creche => creche.selected);
+
+    if (selectedCreches.length === 0) {
+      // Gérer le cas où aucun élément n'est sélectionné
+      console.error('Aucune crèche sélectionnée.');
+      return;
+    }
+
+    const crecheIds = selectedCreches.map(creche => creche.id); // Obtenir uniquement les IDs
+
+    const response = await fetch(`${process.env.VUE_APP_BASEURL}/tournees`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        nom: newTournee.value,
+        creches: crecheIds, // Utiliser uniquement les IDs
+      }),
+    });
+
+    console.log('Données envoyées:', JSON.stringify({
+      nom: newTournee.value,
+      creches: crecheIds, // Utiliser uniquement les IDs
+    }));
+
+    if (response.ok) {
+      // Le reste de votre logique de traitement en cas de succès
+    } else {
+      console.error('Erreur lors de la création de la tournée:', response.status);
+      const errors = await response.json();
+      errorMessage.value = errors;
+    }
+  } catch (error) {
+    console.error('Erreur lors de la création de la tournée:', error);
+  }
+};
+
+
 </script>
-  
 <style scoped>
 .hidden-id {
   display: none;
@@ -128,6 +223,17 @@ td {
 
 #tableHomeView th {
   background-color: #f2f2f2;
+}
+.form-container {
+  margin-top: 20px;
+  background: #fff;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+}
+
+.form-container label {
+  margin-right: 10px;
 }
 </style>
   
