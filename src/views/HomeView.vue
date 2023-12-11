4c57ea75e7ec63a7a213c4411c430f3ac66cf52f
kubraArs <kubra.arslan@student.vinci.be>
2023-12-11 18:43:07 +0100

    propre

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,9 @@
 <template>
   <div class="page-container">
-    <!-- Bouton StockCamion -->
-    <router-link to="/stockcamion">
-      <button v-if="!isadmin" class="btn-StockCamion">Stock Camion</button>
-    </router-link>
-    <!-- Bouton en haut à gauche -->
-    <router-link to="/stockCamionSupp">
-      <button v-if="!isadmin" class="btn-StockCamion">Stock Supplémentaire</button>
-    </router-link>
-    <div class="date-title">
-      <h1>{{ formattedDate }}</h1>
-      <h2>Aujourd'hui</h2>
-    </div>
 
+<ButtonStockCamion></ButtonStockCamion>
     <div class="table-container" id="homeView">
-      <!-- Moteur de recherche -->
-      <div v-if="isadmin" class="container">
-        <input type="text" maxlength="12" placeholder="Rechercher" class="searchbar" />
-      </div>
+
       <div class="table-wrapper" id="homeViewDiv">
         <table id="tableHomeView">
           <thead>
@@ -71,6 +57,7 @@
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import swal from 'sweetalert2';
+import ButtonStockCamion from '@/components/ButtonStockCamion.vue'
 const Swal = ref(swal);
 const router = useRouter();
 
@@ -94,7 +81,6 @@ const fetchData = async () => {
     const response = await fetch(`${process.env.VUE_APP_BASEURL}/users/delivery`, {
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json',
         'Authorization': `Bearer ${accessToken}`,
       },
     });
@@ -149,24 +135,6 @@ const updateFormattedDate = () => {
 };
 </script>
 <style scoped>
-/* Styles pour le bouton de retour */
-/* Styles pour le bouton de retour */
-.btn-StockCamion {
-  width: 80%;
-  height: 40px;
-  border: 1px solid #C4B4FD;
-  background: #B09CFB;
-  color: #fff;
-  cursor: pointer;
-  border-radius: 3px;
-  border-radius: 15px;
-  margin-top: 10px;
-  /* Réduction de la marge en haut */
-  margin-bottom: 10px;
-  /* Réduction de la marge en bas */
-  margin-left: auto;
-  margin-right: auto;
-}
 
 /* Style pour HomeView */
 #homeView {
@@ -188,11 +156,9 @@ const updateFormattedDate = () => {
   margin-top: 2rem;
 }
 
-/* Styles pour le wrapper du tableau */
 #homeViewDiv {
   border: 2px solid #ddd;
   padding: 10px;
-  /* Réduction de la taille du padding */
   border-radius: 10px;
 }
 
@@ -213,62 +179,10 @@ const updateFormattedDate = () => {
   background-color: #f2f2f2;
 }
 
-/* Styles pour les boutons */
-#livraisonCheck {
-  padding: 6px 10px;
-  cursor: pointer;
-  border: none;
-  border-radius: 4px;
-}
 
-#livraisonCheck:hover {
-  opacity: 0.8;
-}
-
-/* Styles pour le titre de la date */
-.date-title {
-  text-align: center;
-  margin-bottom: 10px;
-  /* Réduction de la taille de la marge */
-}
-
-.date-title h2 {
-  font-size: 16px;
-  /* Réduction de la taille de la police */
-  margin: 0;
-}
-
-/* Styles pour la barre de recherche */
 .container {
   width: 100%;
   margin: 0 auto;
-}
-
-.searchbar {
-  font-size: 1rem;
-  /* Réduction de la taille de la police */
-  width: 100%;
-  height: 2.5rem;
-  border: none;
-  outline: none;
-  border-radius: 4rem;
-  padding: 1rem;
-  transition: all 0.1s;
-  transition-delay: 0.1s;
-}
-
-.button {
-  height: 1rem;
-  /* Réduction de la taille de l'icône */
-  position: absolute;
-  top: 0.7rem;
-  right: 0.3rem;
-  transition: all 0.1s;
-  transition-delay: 0.1s;
-}
-
-.searchbar::placeholder {
-  opacity: 0.3;
 }
 
 .hidden-id {
@@ -280,48 +194,26 @@ const updateFormattedDate = () => {
   .table-container {
     width: 100%;
     padding: 20px;
-    /* Ajustement de la taille du padding */
   }
 
   .date-title h1 {
     font-size: 18px;
-    /* Ajustement de la taille de la police */
     margin-bottom: 10px;
-    /* Ajustement de la taille de la marge */
   }
 
   #homeView {
     width: 100%;
     padding: 10px;
-    /* Ajustement de la taille du padding */
     margin-top: 1rem;
-    /* Ajustement de la taille de la marge en haut */
   }
 
   #homeViewDiv {
     padding: 5px;
-    /* Ajustement de la taille du padding */
   }
 
   #tableHomeView th,
   #tableHomeView td {
     padding: 6px;
-    /* Ajustement de la taille du padding */
     font-size: 12px;
-    /* Ajustement de la taille de la police */
-  }
-
-  .searchbar {
-    font-size: 0.9rem;
-    /* Ajustement de la taille de la police */
-  }
-
-  .button {
-    height: 0.8rem;
-    /* Ajustement de la taille de l'icône */
-    top: 0.5rem;
-    /* Ajustement de la position de l'icône */
-    right: 0.2rem;
-    /* Ajustement de la position de l'icône */
   }
 }</style>
